742bf5954030315e953788b2efd61da8854a5de5
Dawn <dmh707@gmail.com>
2022-04-20 22:19:15 -0600

    json

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,23 @@
-
+import * as data from 'scripts/projects_data.json';
+function makeProject(img_url,title,problem_solved,github_url){
+    console.log(img_url,title,problem_solved,github_url)
+}
 
 function readJson(){
-    x = fetch("scripts/projects_data.json")
-        .then(response => response.json())
-        .then(data => console.log(data));
-    console.log(x)
+    for (proj of data){
+        console.log(proj)
+    }
+
 }
 readJson()
 /*
 
+    "a": {
+        "img_url":"../images/proj_example.png",
+        "title":"Project 1",
+        "problem_solved":"Problem I solved",
+        "github_url":"url"
+    },
             <project><a src="#">
                 <img src="images/proj_example.png" class="project-image">
                 <p class="project-title">Project Title</p>
